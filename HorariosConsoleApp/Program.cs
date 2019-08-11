cb96084beec6184757f64a8c2d78a92d1f98774b
Diego Henriquez <df.henriquez@hotmail.com>
2019-08-11 13:35:54 -0500

    Init commit

--- HorariosConsoleApp/Program.cs
+++ HorariosConsoleApp/Program.cs
@@ -2,11 +2,11 @@
 
 namespace HorariosConsoleApp
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Horarios Console App");
         }
     }
 }
