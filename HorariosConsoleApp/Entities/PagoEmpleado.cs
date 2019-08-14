b57d96a69ece3d5ad573e23228f98ebf996a36ae
Diego Henriquez <df.henriquez@hotmail.com>
2019-08-14 14:47:57 -0500

    Added PagoEmpleado

---
+++ HorariosConsoleApp/Entities/PagoEmpleado.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HorariosConsoleApp.Entities
+{
+    public class PagoEmpleado
+    {
+        public int PagoEmpleadoId{ get; set; }
+        public int? EmpleadoId { get; set; }
+        public Empleado Empleado { get; set; }
+        public int? EquipoId { get; set; }
+        public Equipo Equipo { get; set; }
+        public int? HorarioId { get; set; }
+        public Horario Horario { get; set; }
+        public int DiaId { get; set; }
+        public Dia  Dia { get; set; }
+        public DateTime FechaPago { get; set; }
+        public bool EsAsueto { get; set; }
+    }
+}
+
