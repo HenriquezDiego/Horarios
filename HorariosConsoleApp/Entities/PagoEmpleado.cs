c2ea4b72644c46011e5cc322d4e3db2d1738c41c
Diego Henriquez <df.henriquez@hotmail.com>
2019-08-15 14:35:55 -0500

    HotFix PagoEmpleado

--- HorariosConsoleApp/Entities/PagoEmpleado.cs
+++ HorariosConsoleApp/Entities/PagoEmpleado.cs
@@ -2,12 +2,14 @@
 
 namespace HorariosConsoleApp.Entities
 {
+    //Log!!!!
     public class PagoEmpleado
     {
         public int PagoEmpleadoId{ get; set; }
         public int? EmpleadoId { get; set; }
         public Empleado Empleado { get; set; }
         public int? EquipoId { get; set; }
+        public decimal SalarioHora { get; set; }
         public Equipo Equipo { get; set; }
         public int? HorarioId { get; set; }
         public Horario Horario { get; set; }
@@ -15,6 +17,23 @@ namespace HorariosConsoleApp.Entities
         public Dia  Dia { get; set; }
         public DateTime FechaPago { get; set; }
         public bool EsAsueto { get; set; }
+        public double HorasOrdinariasDiurnas { get; set; }
+        public double TotalHorasOrdinariasDiurnas { get; set; }
+        public double HorasOrdinariasNocturnas { get; set; }
+        public double TotalHorasOrdinariasNocturnas { get; set; }
+        public double HorasExtraDiurnas { get; set; }
+        public double TotalHorasExtraDiurnas { get; set; }
+        public double HorasExtraNocturnas { get; set; }
+        public double TotalHorasExtraNocturnas { get; set; }
+        public double HorasOrinariasDiurnasDomingo { get; set; }
+        public double TotalHorasOrinariasDiurnasDomingo { get; set; }
+        public double HorasOrdinarioasNocturnasDomingo { get; set; }
+        public double TotalHorasOrdinariasNocturnasDomingo { get; set; }
+        public double HorasExtraDiurnasDomingo { get; set; }
+        public double TotalHorasExtraDiurnasDomingo { get; set; }
+        public double HorasExtraNocturnasDomingo { get; set; }
+        public double TotalHorasExtraNocturnasDomingo { get; set; }
+        public double TotalAsueto { get; set; }
+
     }
 }
-
